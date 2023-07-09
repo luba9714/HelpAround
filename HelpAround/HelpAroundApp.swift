b4e6bb692cc7137720c6a9d4af55c51f38a600a7
lubagluhov <luba9714@gmail.com>
2023-07-09 09:57:37 +0300

    Initial Commit

---
+++ HelpAround/HelpAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpAroundApp.swift
+//  HelpAround
+//
+//  Created by Luba Gluhov on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelpAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
